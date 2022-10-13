c2c2916e6a668cf5cb547e5c53ef7fbcfecc670d
chengxu xu <jason_xu@chengxudeMacBook-Pro.local>
2022-10-12 21:23:12 -0700

    Discover

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-8xl font-bold underline">
+    <h1 className="text-4xl font-bold underline">
       Hello world!
     </h1>
   )
