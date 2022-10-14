98c0216f7a9c9818b88f648f7b4269ce46f7da35
xcxxuri <1157850364@qq.com>
2022-10-14 15:58:15 -0700

    Google Oahtu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
 import { AiOutlineLogout } from 'react-icons/ai'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
 import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
+  const user = false
+
   return (
     <div className="w-full flex justify-between 
     items-center border-b-2 
@@ -24,6 +26,19 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>SEARCH</div>
+
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log("error")}
+          />
+        )}
+
+      </div>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -15,9 +15,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   if (isSSR) return null;
 
-
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -27,7 +26,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
