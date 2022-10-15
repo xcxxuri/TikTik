18903bf8d6f3b838231c821541b4e5f6b33da82f
xcxxuri <1157850364@qq.com>
2022-10-14 21:21:57 -0700

    User Store

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,12 @@ import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
 import Logo from '../utils/tiktik-logo.png'
+import { createOrGetUser } from '../utils'
+
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
-  const user = false
+  const { userProfile, addUser, removeUser } = useAuthStore()
 
   return (
     <div className="w-full flex justify-between 
@@ -29,11 +32,42 @@ const Navbar = () => {
       <div>SEARCH</div>
 
       <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div className='flex gap-5 md:gap-10'>
+            <Link href="/upload">
+              < button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {` `}
+                <samp className="hidden md:block">Upload</samp>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="profile photo"
+
+                  />
+                </>
+              </Link>
+            )}
+            <button
+              type="button"
+              className="px-2"
+              onClick={() => {
+                googleLogout()
+                removeUser()
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
         ) : (
           <GoogleLogin
-            onSuccess={(response) => console.log(response)}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log("error")}
           />
         )}
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,8 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { client } from '../../../utils/client'
 import { allPostsQuery } from '../../../utils/queries'
-
 
 
 export default async function handler(req: NextApiRequest,
